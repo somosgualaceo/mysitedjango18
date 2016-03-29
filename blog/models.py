911b35944f8152d7716f855a1822657826573c20
Jose Luis <somosgualaceo@gmail.com>
2016-03-28 22:53:05 -0500

    subir

--- blog/models.py
+++ blog/models.py
@@ -12,5 +12,5 @@ class Post(models.Model):
     	self.published_date = timezone.now()
     	self.save()
 
-        def __unicode__(self):
-            return self.title
+    def __unicode__(self):
+        return self.title
