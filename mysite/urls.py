deb30393b7d1b544e5c3fd21cc250a0bd16b87c6
Jose Luis <somosgualaceo@gmail.com>
2016-03-28 21:08:23 -0500

    cabron

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,10 @@
 
 from django.conf.urls import include , url
 from django.contrib import admin
-
+import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT}),
 ]
