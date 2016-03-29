d73361ec118ce078173feb21ca009b8a9a5c7871
Jose Luis <somosgualaceo@gmail.com>
2016-03-29 09:51:13 -0500

    paginador

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,22 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-	return render(request, 'blog/post_list.html', {'posts': posts})
+
+	paginator = Paginator(posts, 3) 
+	page = request.GET.get('page')
+
+	try:
+		list = paginator.page(page)
+	except PageNotAnInteger:
+		list = paginator.page(1)
+	except EmptyPage:
+		list = paginator.page(paginator.num_pages)
+
+	return render(request, 'blog/post_list.html', {'posts': posts ,'posts': list })
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
